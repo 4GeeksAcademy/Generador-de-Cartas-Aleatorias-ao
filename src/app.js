bcb86dcedd1d568dfc12350d1eab290b5933e13e
Alvaroorta1 <165050201+Alvaroorta1@users.noreply.github.com>
2024-05-16 00:43:29 +0000

    segundo commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardValue = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const cardType = ["♦", "♥", "♠", "♣"];
+  const randomType = Math.floor(Math.random() * cardType.length);
+  const randomValue = Math.floor(Math.random() * cardValue.length);
+  const randomCardValue = cardValue[randomValue];
+  const randomCardType = cardType[randomType];
+  console.log(randomCardType);
+  console.log(randomCardValue);
+
+  document.querySelector(".card-number").innerHTML = randomCardValue;
+  document.querySelector(".card-up-side").innerHTML = randomCardType;
+  document.querySelector(".card-symbol").innerHTML = randomCardType;
 };
